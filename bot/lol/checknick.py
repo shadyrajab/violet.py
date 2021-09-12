928d7688489e176fc72db7643bc0726a2e8fe451
shady <shadyrajaab@gmail.com>
2021-09-12 11:29:45 -0300

    .

--- bot/lol/checknick.py
+++ bot/lol/checknick.py
@@ -12,7 +12,7 @@ class CheckNick(commands.Cog):
 
     @commands.command()
     async def lolchecknick(self, ctx, server, *, username):
-        summoner = requests.get(f'https://{server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}?api_key=RGAPI-eaf140b8-79c7-41c3-8958-e4a3e3a731bd').json()
+        summoner = requests.get(f'https://{server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{username}?api_key={getenv("RGKEY")}').json()
         matchs = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{summoner["puuid"]}/ids?start=0&count=20&api_key={getenv("RGKEY")}').json()
         last_match = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/{matchs[0]}?api_key={getenv("RGKEY")}').json()
         data = datetime.fromtimestamp(last_match["info"]["gameStartTimestamp"] / 1e3)
